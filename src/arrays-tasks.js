359732e986fb45138b827c68a3cf7a1f75783163
wingedseraph <r89647@gmail.com>
2024-02-11 17:01:13 +0300

    feat: done 30 functions

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -178,8 +178,11 @@ function isSameLength(arr) {
  *    isValueEqualsIndex([2, 1, 0, 4, 5]) => true
  *    isValueEqualsIndex([10, 20, 30, 40, 50]) => false
  */
-function isValueEqualsIndex(/* arr */) {
-  throw new Error('Not implemented');
+function isValueEqualsIndex(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error('input must be array');
+  }
+  return arr.some((value, index) => value === index);
 }
 
 /**
@@ -193,8 +196,11 @@ function isValueEqualsIndex(/* arr */) {
  *    insertItem([ 1, 3, 4, 5 ], 2, 1)  => [ 1, 2, 3, 4, 5 ]
  *    insertItem([ 1, 'b', 'c'], 'x', 0) => [ 'x', 1, 'b', 'c' ]
  */
-function insertItem(/* arr, item, index */) {
-  throw new Error('Not implemented');
+function insertItem(arr, item, index) {
+  if (!Array.isArray(arr) || index < 0 || index > arr.length) {
+    throw new Error('input must be array');
+  }
+  return arr.splice(index, 0, item);
 }
 
 /**
@@ -208,8 +214,11 @@ function insertItem(/* arr, item, index */) {
  *    getHead([ 'a', 'b', 'c', 'd'], 3) => [ 'a', 'b', 'c' ]
  *    getHead([ 'a', 'b', 'c', 'd'], 0) => []
  */
-function getHead(/* arr, n */) {
-  throw new Error('Not implemented');
+function getHead(arr, n) {
+  if (!Array.isArray(arr)) {
+    throw new Error('input must be array');
+  }
+  return arr.slice(0, n);
 }
 
 /**
@@ -223,8 +232,12 @@ function getHead(/* arr, n */) {
  *    getTail([ 'a', 'b', 'c', 'd'], 3) => [ 'b', 'c', 'd' ]
  *    getTail([ 'a', 'b', 'c', 'd'], 0) => []
  */
-function getTail(/* arr, n */) {
-  throw new Error('Not implemented');
+function getTail(arr, n) {
+  if (!Array.isArray(arr) || n < 0) {
+    throw new Error('input must be array and n > 0');
+  }
+  if (n === 0) return [];
+  return arr.slice(-n);
 }
 
 /**
@@ -239,8 +252,11 @@ function getTail(/* arr, n */) {
  *    doubleArray([0, 1, 2, 3, 4, 5]) => [0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5]
  *    doubleArray([]) => []
  */
-function doubleArray(/* arr */) {
-  throw new Error('Not implemented');
+function doubleArray(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error('input must be array');
+  }
+  return arr.concat(arr);
 }
 
 /**
@@ -254,8 +270,11 @@ function doubleArray(/* arr */) {
  *    toStringList([1, 2, 3, 4, 5]) => '1,2,3,4,5'
  *    toStringList(['rock', 'paper', 'scissors']) => 'rock,paper,scissors'
  */
-function toStringList(/* arr */) {
-  throw new Error('Not implemented');
+function toStringList(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error('input must be array');
+  }
+  return arr.join(',');
 }
 
 /**
@@ -270,8 +289,11 @@ function toStringList(/* arr */) {
  *   distinct([ 1, 1, 2, 2, 3, 3, 4, 4]) => [ 1, 2, 3, 4]
  *   distinct([]) => []
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error('input must be array');
+  }
+  return Array.from(new Set(arr));
 }
 
 /**
@@ -287,8 +309,18 @@ function distinct(/* arr */) {
  *    createNDimensionalArray(4, 2) => [[[[0, 0], [0, 0]], [[0, 0], [0, 0]]], [[[0, 0], [0, 0]], [[0, 0], [0, 0]]]]
  *    createNDimensionalArray(1, 1) => [0]
  */
-function createNDimensionalArray(/* n, size */) {
-  throw new Error('Not implemented');
+function createNDimensionalArray(n, size) {
+  if (n <= 0 || size <= 0) {
+    throw new Error('n and size must be > 0');
+  }
+  const createArray = (dimension) => {
+    if (dimension === 1) {
+      return Array(size).fill(0);
+    }
+    return Array(size).fill(createArray(dimension - 1));
+  };
+
+  return createArray(n);
 }
 
 /**
